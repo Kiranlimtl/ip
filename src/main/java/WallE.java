accbd576fd72084901f422016062379ba7abf76b
Kiranlimtl <kiranlimtl@gmail.com>
2025-01-30 20:40:56 +0800

    Level-2. Add, List

--- src/main/java/WallE.java
+++ src/main/java/WallE.java
@@ -5,32 +5,30 @@ public class WallE {
         //Create new scanner
         Scanner scanner = new Scanner(System.in);
 
+        //Create 100 element String Array
+        String[] tasks = new String[100];
+        int curr = 0;
+
         //Greetings
-        System.out.println("\tHello! I'm WallE.");
-        System.out.println("\tWhat can I do for you?");
-        System.out.println();
-        printHorizontalLine();
-        System.out.println();
-        System.out.println();
+        printWithLine("Hello! I'm WallE.\n" +
+                "\tWhat can I do for you?");
 
         while (true) {
             String userInput = scanner.nextLine();
 
             // Check if the user wants to exit
             if (userInput.equalsIgnoreCase("bye")) {
-                printHorizontalLine();
-                System.out.println("\tBye. Hope to see you again soon!");
-                printHorizontalLine();
+                printWithLine("Bye. Hope to see you again soon!");
                 break;
             }
+            if (userInput.equalsIgnoreCase("list")) {
+                printTasks(tasks, curr);
+            } else {
+                tasks[curr] = userInput;
+                curr++;
+                printWithLine("added: " + userInput);
+            }
 
-            // Echo the user's input
-            printHorizontalLine();
-            System.out.println("\t" + userInput);
-            System.out.println();
-            printHorizontalLine();
-            System.out.println();
-            System.out.println();
         }
 
         //Close the scanner
@@ -41,5 +39,29 @@ public class WallE {
     private static void printHorizontalLine(){
         System.out.println("\t--------------------------------------------------");
     }
+
+    //Method to print other stuff with line
+    private static void printWithLine(String input){
+        printHorizontalLine();
+        System.out.println("\t" + input);
+        System.out.println();
+        printHorizontalLine();
+        System.out.println();
+    }
+
+    private static void printTasks(String[] tasks, int curr){
+        if (curr == 0) {
+            printWithLine("No tasks found");
+        } else {
+            printHorizontalLine();
+            for (int i = 0; i < curr; i++){
+                System.out.println("\t" + (i + 1) + ". " + tasks[i]);
+            }
+            System.out.println();
+            printHorizontalLine();
+        }
+
+    }
+
 
 }
