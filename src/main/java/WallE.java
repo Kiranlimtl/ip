abf5494d485e9cd8aa6478fc86f426f2aa49ed53
Kiranlimtl <kiranlimtl@gmail.com>
2025-01-30 20:04:13 +0800

    Level-1. Echo

--- src/main/java/WallE.java
+++ src/main/java/WallE.java
@@ -1,9 +1,45 @@
+import java.util.Scanner;
+
 public class WallE {
     public static void main(String[] args) {
+        //Create new scanner
+        Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Hello! I'm WallE\n" +
-                "What can I do for you?\n" +
-                "\n" +
-                "Bye. Hope to see you again soon!");
+        //Greetings
+        System.out.println("\tHello! I'm WallE.");
+        System.out.println("\tWhat can I do for you?");
+        System.out.println();
+        printHorizontalLine();
+        System.out.println();
+        System.out.println();
+
+        while (true) {
+            String userInput = scanner.nextLine();
+
+            // Check if the user wants to exit
+            if (userInput.equalsIgnoreCase("bye")) {
+                printHorizontalLine();
+                System.out.println("\tBye. Hope to see you again soon!");
+                printHorizontalLine();
+                break;
+            }
+
+            // Echo the user's input
+            printHorizontalLine();
+            System.out.println("\t" + userInput);
+            System.out.println();
+            printHorizontalLine();
+            System.out.println();
+            System.out.println();
+        }
+
+        //Close the scanner
+        scanner.close();
     }
+
+    //Method to print horizontal line
+    private static void printHorizontalLine(){
+        System.out.println("\t--------------------------------------------------");
+    }
+
 }
