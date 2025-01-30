5cde3a18a3560efe10879c97971fb1b810afaf42
Kiranlimtl <kiranlimtl@gmail.com>
2025-01-30 19:39:14 +0800

    Level-0

---
+++ src/main/java/WallE.java
@@ -0,0 +1,9 @@
+public class WallE {
+    public static void main(String[] args) {
+
+        System.out.println("Hello! I'm WallE\n" +
+                "What can I do for you?\n" +
+                "\n" +
+                "Bye. Hope to see you again soon!");
+    }
+}
